2ef5c5a669a703487ceb820da4785275f7230ef0
Pavlo Manuilenko <manuilenkopavel@gmail.com>
2024-05-03 16:12:18 +0300

    Enriched README.md Uncommented ExceptionHandlers

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 User Validation.postman_collection.json contains collection that you can import into postman and test Web API Validation.
+For application run you should set profiles, specifically "dev":
+1. Go to Intellij configuration
+2. Choose "Modify options" and select "Add VM options"
+3. insert into appeared field "-Dspring.profiles.active=dev"
+Now you can run application using "dev" profile
+For testing other part of Web Api you can you jsondoc:
+1. Run application
+2. Go to http://localhost:8080/jsondoc-ui.html
+3. insert http://localhost:8080/jsondoc in the box and get the documentation.
+
--- src/main/java/com/example/assignment/web/controller/RestGlobalExceptionHandler.java
+++ src/main/java/com/example/assignment/web/controller/RestGlobalExceptionHandler.java
@@ -17,6 +17,8 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestGlobalExceptionHandler {
+    private static final String VALIDATION_TITLE = "Validation failed";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<FieldDetail> fieldDetailList = e.getFieldErrors()
@@ -24,24 +26,22 @@ public class RestGlobalExceptionHandler {
                 .map(fieldError -> new FieldDetail(fieldError.getField(), fieldError.getDefaultMessage()))
                 .sorted(Comparator.comparing(FieldDetail::getFieldName).thenComparing(FieldDetail::getMessage))
                 .collect(Collectors.toList());
-        String title = "Validation failed";
-        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), title, fieldDetailList);
+        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), VALIDATION_TITLE, fieldDetailList);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 
     }
 
-//    @ExceptionHandler(ConstraintViolationException.class)
-//    public ResponseEntity<?> onConstraintViolationException(ConstraintViolationException e) {
-//        List<FieldDetail> fieldDetailList = e.getConstraintViolations()
-//                .stream()
-//                .map(fieldError -> new FieldDetail(fieldError.getPropertyPath().toString(), fieldError.getMessage()))
-//                .sorted(Comparator.comparing(FieldDetail::getFieldName).thenComparing(FieldDetail::getMessage))
-//                .collect(Collectors.toList());
-//        String title = "Validation failed";
-//        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), title, fieldDetailList);
-//        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//
-//    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> onConstraintViolationException(ConstraintViolationException e) {
+        List<FieldDetail> fieldDetailList = e.getConstraintViolations()
+                .stream()
+                .map(fieldError -> new FieldDetail(fieldError.getPropertyPath().toString(), fieldError.getMessage()))
+                .sorted(Comparator.comparing(FieldDetail::getFieldName).thenComparing(FieldDetail::getMessage))
+                .collect(Collectors.toList());
+        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), VALIDATION_TITLE, fieldDetailList);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<?> onBindException(BindException e) {
@@ -50,8 +50,7 @@ public class RestGlobalExceptionHandler {
                 .map(fieldError -> new FieldDetail(fieldError.getField(), fieldError.getDefaultMessage()))
                 .sorted(Comparator.comparing(FieldDetail::getFieldName).thenComparing(FieldDetail::getMessage))
                 .collect(Collectors.toList());
-        String title = "Validation failed";
-        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), title, fieldDetailList);
+        ErrorResponse<List<FieldDetail>> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), VALIDATION_TITLE, fieldDetailList);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -62,11 +61,11 @@ public class RestGlobalExceptionHandler {
 
     }
 
-//    @ExceptionHandler(RuntimeException.class)
-//    public ResponseEntity<?> onRuntimeException(RuntimeException e) {
-//        ErrorResponse<?> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), e.getMessage(), null);
-//        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-//
-//    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> onRuntimeException(RuntimeException e) {
+        ErrorResponse<?> errorResponse = new ErrorResponse<>(HttpStatus.BAD_REQUEST.value(), e.getMessage(), null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
 
 }
